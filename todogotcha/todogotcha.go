f41694153aa0527a605512653d5afb1001b910e3
dory <empty@empty>
2016-09-23 17:19:23 +0900

    fix functions On branch master Changes to be committed: deleted:    bench.txt deleted:    todogotcha/bench.txt modified:   todogotcha/todogotcha.go

--- todogotcha/todogotcha.go
+++ todogotcha/todogotcha.go
@@ -23,6 +23,7 @@ var (
 	suffix       = flag.String("filetype", "go txt", `Specify target file type into the " "`)
 	suffixList   []string
 	gatherTarget = flag.String("key", "TODO:", "Specify gather target keyword")
+	result       = flag.String("result", "yes", "Specify result [yes:no]?")
 )
 
 func init() {
@@ -53,7 +54,7 @@ func argsCheck() {
 
 // Use wait group!!
 // もう少しシンプルにしたい
-func dirsCrawl(root string) (map[string][]os.FileInfo) {
+func dirsCrawl(root string) map[string][]os.FileInfo {
 	// mux group
 	DirsCache := make(map[string]bool)
 	InfoCache := make(map[string][]os.FileInfo)
@@ -67,14 +68,15 @@ func dirsCrawl(root string) (map[string][]os.FileInfo) {
 
 		Mux.Lock()
 		if DirsCache[dirname] {
+			Mux.Unlock()
 			return
 		}
 		DirsCache[dirname] = true
-		Mux.Unlock()
 
 		f, err := os.Open(dirname)
 		if err != nil {
-			log.Printf("crawl:%v\n", err)
+			log.Printf("crawl:%v", err)
+			Mux.Unlock()
 			return
 		}
 		defer f.Close()
@@ -82,16 +84,18 @@ func dirsCrawl(root string) (map[string][]os.FileInfo) {
 		info, err := f.Readdir(0)
 		if err != nil {
 			log.Printf("crawl info:%v", err)
+			Mux.Unlock()
 			return
 		}
-		Mux.Lock()
 		InfoCache[dirname] = info
-		Mux.Unlock()
 
 		// NOTE:countermove for too many open files
 		if err := f.Close(); err != nil {
 			log.Println(err)
 		}
+		Mux.Unlock()
+		// "too many open files" の対応でlockしたけど...
+		// ここまでlockするならスレッド分ける意義が薄そう...
 
 		for _, x := range info {
 			if x.IsDir() {
@@ -104,7 +108,6 @@ func dirsCrawl(root string) (map[string][]os.FileInfo) {
 	Wg.Add(1)
 	crawl(root)
 	Wg.Wait()
-	// DirsList は range InfoCache で取得できるので省いた方がいいかも
 	return InfoCache
 }
 
@@ -119,21 +122,18 @@ func suffixSeacher(filename string, targetSuffix []string) bool {
 }
 
 // specify filename and target, Gather target(TODOs), return todoList.
-func gather(filename string, target string) ([]string, error) {
-	todoList := []string{target}
+// シンプルでいい感じに見えるけど、goroutineで呼びまくると...(´・ω・`)っ"too many open files"
+func gather(filename string, target string) (todoList []string, err error) {
+	todoList = []string{target}
 	f, err := os.Open(filename)
 	if err != nil {
-		return nil, fmt.Errorf("todoGather:%v\n", err)
+		return nil, fmt.Errorf("todoGather:%v", err)
 	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			log.Printf("todoGather:%v", err)
-		}
-	}()
+	defer f.Close()
 
 	for sc, i := bufio.NewScanner(f), uint(1); sc.Scan(); i++ {
 		if err := sc.Err(); err != nil {
-			return nil, fmt.Errorf("todoGather:%v\n", err)
+			return nil, fmt.Errorf("todoGather:%v", err)
 		}
 		if index := strings.Index(sc.Text(), target); index != -1 {
 			todoList = append(todoList, fmt.Sprintf("L%v:%s", i, sc.Text()[index+len(target):]))
@@ -148,32 +148,34 @@ func gather(filename string, target string) ([]string, error) {
 
 // にゃん
 func unlimitedGophersWroks(infoMap map[string][]os.FileInfo) (todoMap map[string][]string) {
-	// need use goroutine
+	// TODO:Use goroutine
 	todoMap = make(map[string][]string)
-	mux := new(sync.Mutex)
-	wg := new(sync.WaitGroup)
+
+	//mux := new(sync.Mutex)
+	//wg := new(sync.WaitGroup)
 
 	worker := func(filepath string) {
-		defer wg.Done()
+		//defer wg.Done()
+
 		// TODO:!!
 		// ファイルが多いとosのファイルディスクリプタ上限で叩かれるぅ...どうしよ...
 		// スレッドを増やしまくるgoだとよくハマるっぽい
 		// wgでカウント取って上限でwaitできれば良さそうだけど、カウンタは公開されてない
-		// muxと自前のカウンタで何とかするか...ちょっと考える
+		// muxと自前のカウンタで多分行ける...
 		// 取り敢えず簡単にカウント取って一定値でwaitする?
 		// os環境個別のディスクリプタ上限を取得する関数とかあれば管理できそうだけど見つかってぬぃ...
 		// これをクリアできないと大量のファイルを同時に走査するマルチスレッド使えない...
 		// 新しいルーチン呼ぶ前にファイルの内容をバッファに投げてクローズすれば良さそう?
-		// バッファがメモリ圧迫しそう...
-		// strings.Newreader(tmp)を渡そう...
+		// NewReader()なんかは結局ファイルディスクリプタ使っちゃってるっぽいから考えないといけぬぃ...
+		// 取り敢えずシングルスレッドで置いておく
 		todoList, err := gather(filepath, *gatherTarget)
 		if err != nil {
 			log.Println(err)
 		}
 		if todoList != nil {
-			mux.Lock()
+			//mux.Lock()
 			todoMap[filepath] = todoList
-			mux.Unlock()
+			//mux.Unlock()
 		}
 	}
 
@@ -181,12 +183,13 @@ func unlimitedGophersWroks(infoMap map[string][]os.FileInfo) (todoMap map[string
 		for _, info := range infos {
 			if suffixSeacher(info.Name(), suffixList) {
 				// TODO:file open and close to tmp string
-				wg.Add(1)
-				go worker(filepath.Join(dirname, info.Name()))
+				//wg.Add(1)
+				//go worker(filepath.Join(dirname, info.Name()))
+				worker(filepath.Join(dirname, info.Name()))
 			}
 		}
 	}
-	wg.Wait()
+	//wg.Wait()
 	return todoMap
 }
 func useGophersProc() (todoMap map[string][]string) {
@@ -195,11 +198,38 @@ func useGophersProc() (todoMap map[string][]string) {
 	return todoMap
 }
 
+// output to os.Stdout!
+func outputTODOList(todoMap map[string][]string) {
+	for filename, list := range todoMap {
+		fmt.Println(filename)
+		for _, s := range list {
+			fmt.Println(s)
+		}
+		fmt.Println()
+	}
+	if *result == "yes" {
+		fmt.Println("-----| RESULT |-----")
+		fmt.Printf("Find %v files\n", len(todoMap))
+		fmt.Printf("Search root=%q\n", *root)
+		fmt.Printf("Search wrod=%q\n", *gatherTarget)
+		fmt.Printf("Target filetypes%q\n", *suffix)
+	}
+}
+func main() {
+
+	// TODO:Erase after test
+	//	todoMap, err := mainproc()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+
+	todoMap := useGophersProc()
+	outputTODOList(todoMap)
+}
+
 // TODOGotcha!! main proc
-// 驚くべき読みにくさ...何とかしたい
-// 多分データ構造の選択を間違えてる
 // TODO:erase after imprementertion to goroutine procs!!
-// do not used
+// This function is no used now
 func mainproc() (todoMap map[string][]string, gatherErr error) {
 	todoMap = make(map[string][]string)
 	infomap := dirsCrawl(*root)
@@ -218,26 +248,4 @@ func mainproc() (todoMap map[string][]string, gatherErr error) {
 		}
 	}
 	return todoMap, gatherErr
-}
-
-// output list!
-func outputTODOList(todoMap map[string][]string) {
-	for filename, list := range todoMap {
-		fmt.Println(filename)
-		for _, s := range list {
-			fmt.Println(s)
-		}
-		fmt.Println()
-	}
-	fmt.Printf("stack files=%v\n", len(todoMap))
-}
-func main() {
-
-	//	todoMap, err := mainproc()
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-
-	todoMap := useGophersProc()
-	outputTODOList(todoMap)
 }

f5ea99e9a3343ca93693ddbd3367897274a18ee3
yaeshimo <hatsushimoyae@gmail.com>
2019-04-05 21:43:46 +0900

    modify *Walker.Run to *Walker.Start

--- walker.go
+++ walker.go
@@ -16,7 +16,14 @@ import (
 )
 
 var ErrInvalidText = errors.New("unavailable encoding")
-var ErrInternal = errors.New("internal error")
+
+type InternalError struct {
+	e error
+}
+
+func (ei *InternalError) Error() string {
+	return fmt.Sprintf("internal error:%v", ei.e)
+}
 
 type Line struct {
 	Num uint
@@ -32,10 +39,8 @@ func NewLineQueue(capacity uint) (*LineQueue, error) {
 	}
 	return &LineQueue{make(chan *Line, capacity)}, nil
 }
-
 func (lq *LineQueue) Len() int { return len(lq.q) }
 func (lq *LineQueue) Cap() int { return cap(lq.q) }
-
 func (lq *LineQueue) Push(l *Line) {
 	select {
 	case lq.q <- l:
@@ -44,7 +49,6 @@ func (lq *LineQueue) Push(l *Line) {
 		lq.q <- l
 	}
 }
-
 func (lq *LineQueue) PopAll() []*Line {
 	lines := make([]*Line, 0, cap(lq.q))
 	for len(lq.q) != 0 {
@@ -53,7 +57,7 @@ func (lq *LineQueue) PopAll() []*Line {
 	return lines
 }
 
-// change to struct{ raw []*Line, index int, pos int }?
+// change to struct{ index int, pos int, lines []*Line }?
 type Context struct {
 	line   *Line
 	before []*Line
@@ -133,187 +137,182 @@ type Walker struct {
 	dirQueue  chan []string
 
 	regexp *regexp.Regexp
-	files  map[string]*File
 
-	log, errlog   *log.Logger
 	nworker       int
+	checked       map[string]bool
 	mu            sync.Mutex
 	wg            sync.WaitGroup
 	once          sync.Once
-	internalError bool
+	internalError *InternalError
+
+	log *log.Logger
 }
 
 func NewWalker() *Walker {
-	nworker := runtime.NumCPU() / 2
+	nworker := runtime.NumCPU() / 4
 	if nworker < 2 {
 		nworker = 2
 	}
 	return &Walker{
-		fileQueue: make(chan string, 256),
-		dirQueue:  make(chan []string, 128),
-		nworker:   nworker,
-		files:     make(map[string]*File),
-		log:       log.New(ioutil.Discard, Name+":", 0),
-		errlog:    log.New(ioutil.Discard, Name+":[Err]:", 0),
+		fileQueue:     make(chan string, 128),
+		dirQueue:      make(chan []string, nworker),
+		nworker:       nworker,
+		checked:       make(map[string]bool),
+		log:           log.New(ioutil.Discard, Name+":", 0),
+		internalError: nil,
 	}
 }
 
-func (w *Walker) SetLog(writer io.Writer)    { w.log.SetOutput(writer) }
-func (w *Walker) SetErrLog(writer io.Writer) { w.errlog.SetOutput(writer) }
+func (w *Walker) SetLogOutput(writer io.Writer) { w.log.SetOutput(writer) }
 
-func (w *Walker) setInternalError() {
-	w.once.Do(func() { w.internalError = true })
+func (w *Walker) setInternalError(err error) {
+	w.once.Do(func() { w.internalError = &InternalError{e: err} })
 }
 
-// may change verbose
-func (w *Walker) run(writer io.Writer, verbose bool, pat string, lines uint, paths ...string) error {
-	re, err := regexp.Compile(pat)
-	if err != nil {
-		return err
-	}
-	w.regexp = re
-
-	go w.dirWalker()
-	var doResult func(*File)
-	if writer == nil {
-		doResult = func(f *File) {
-			w.mu.Lock()
-			w.files[f.path] = f
-			w.mu.Unlock()
-		}
-	} else {
-		var printFunc func(io.Writer, *File) error
-		if verbose {
-			printFunc = FprintFileVerbose
-		} else {
-			printFunc = FprintFile
-		}
-		var rwm sync.RWMutex
-		doResult = func(f *File) {
-			rwm.Lock()
-			err := printFunc(writer, f)
-			rwm.Unlock()
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-	for i := 0; i != w.nworker; i++ {
-		go w.fileWalker(lines, doResult)
-	}
-
-	// paths to abs?
-	// treat symlinks?
-	// limitation of depth?
+func (w *Walker) sendQueue(paths ...string) {
 	var dirs []string
-	for _, path := range paths {
-		fi, err := os.Stat(path)
+	for i := range paths {
+		abs, err := filepath.Abs(paths[i])
 		if err != nil {
-			return err
+			w.setInternalError(err)
+			w.log.Printf("[Err]:%v", err)
+			continue
 		}
-		switch {
-		case fi.Mode().IsRegular():
+		fi, err := os.Stat(abs)
+		if err != nil {
+			w.setInternalError(err)
+			w.log.Printf("[Errr]:%v", err)
+			continue
+		}
+		if fi.IsDir() {
+			dirs = append(dirs, abs)
+		} else if fi.Mode().IsRegular() {
 			w.wg.Add(1)
-			w.fileQueue <- path
-		case fi.IsDir():
-			dirs = append(dirs, path)
+			w.fileQueue <- abs
 		}
 	}
 	w.wg.Add(1)
 	w.dirQueue <- dirs
-	w.wg.Wait()
+}
 
-	if w.internalError {
-		return ErrInternal
+func (w *Walker) Start(pat string, nlines uint, paths ...string) (<-chan *File, func() error, error) {
+	var re *regexp.Regexp
+	re, err := regexp.Compile(pat)
+	if err != nil {
+		return nil, nil, err
 	}
-	return nil
-}
+	w.regexp = re
 
-func (w *Walker) Run(pat string, lines uint, paths ...string) error {
-	return w.run(nil, false, pat, lines, paths...)
-}
+	resultQueue := make(chan *File, cap(w.fileQueue))
+	done := make(chan struct{})
+	wait := func() error {
+		w.wg.Wait()
+		return w.internalError
+	}
 
-func (w *Walker) RunSyncWrite(writer io.Writer, verbose bool, pat string, lines uint, paths ...string) error {
-	return w.run(writer, verbose, pat, lines, paths...)
+	for i := 0; i != w.nworker; i++ {
+		go w.dirWalker(done)
+		go w.fileWalker(done, resultQueue, nlines)
+	}
+	w.sendQueue(paths...)
+	go func() {
+		w.wg.Wait()
+		close(done)
+		close(resultQueue)
+	}()
+	return resultQueue, wait, nil
 }
 
 // for goroutine
 // send tasks to {file,dir}Queue
-func (w *Walker) dirWalker() {
+func (w *Walker) dirWalker(done <-chan struct{}) {
 	var nextDirs []string
+	var dirs []string
 	for ; true; w.wg.Done() {
-		dirs := <-w.dirQueue
-		for _, dir := range dirs {
-			fis, err := ioutil.ReadDir(dir)
+		select {
+		case dirs = <-w.dirQueue:
+			for i := range dirs {
+				fis, err := ioutil.ReadDir(dirs[i])
+				if err != nil {
+					w.setInternalError(err)
+					w.log.Printf("[Err]:%s:%v", dirs[i], err)
+					if os.IsNotExist(err) || os.IsPermission(err) {
+						continue
+					}
+					// unexpected error
+					panic(err)
+				}
+				for _, fi := range fis {
+					if fi.Mode().IsRegular() {
+						w.wg.Add(1)
+						w.fileQueue <- filepath.Join(dirs[i], fi.Name())
+					} else if fi.IsDir() {
+						nextDirs = append(nextDirs, filepath.Join(dirs[i], fi.Name()))
+					}
+				}
+			}
+			if nextDirs != nil {
+				w.wg.Add(1)
+				w.dirQueue <- nextDirs
+				nextDirs = nil
+			}
+		case <-done:
+			return
+		}
+	}
+}
+
+// for goroutine
+func (w *Walker) fileWalker(done <-chan struct{}, resultQueue chan<- *File, nlines uint) {
+	var lq *LineQueue
+	if nlines != 0 {
+		// not need error check
+		lq, _ = NewLineQueue(nlines)
+	}
+	var file string
+	var err error
+	var cs []*Context
+	for ; true; w.wg.Done() {
+		select {
+		case file = <-w.fileQueue:
+			w.mu.Lock()
+			if w.checked[file] {
+				w.mu.Unlock()
+				continue
+			}
+			w.checked[file] = true
+			w.mu.Unlock()
+
+			cs, err = w.readFile(file, lq)
 			if err != nil {
-				w.setInternalError()
-				w.errlog.Printf("%q:%v", dir, err)
+				w.setInternalError(err)
+				w.log.Printf("[Err]:%s:%v", file, err)
 				if os.IsNotExist(err) || os.IsPermission(err) {
+					continue
+				}
+				if err == bufio.ErrTooLong {
+					continue
+				}
+				if err == ErrInvalidText {
 					continue
 				}
 				// unexpected error
 				panic(err)
 			}
-			for _, fi := range fis {
-				switch {
-				case fi.Mode().IsRegular():
-					w.wg.Add(1)
-					w.fileQueue <- filepath.Join(dir, fi.Name())
-				case fi.IsDir():
-					nextDirs = append(nextDirs, filepath.Join(dir, fi.Name()))
+			w.log.Println(file)
+			if len(cs) != 0 {
+				resultQueue <- &File{
+					path: file,
+					cs:   cs,
 				}
 			}
-		}
-		if nextDirs != nil {
-			w.wg.Add(1)
-			w.dirQueue <- nextDirs
-			nextDirs = nil
+		case <-done:
+			return
 		}
 	}
 }
 
-// for goroutine
-func (w *Walker) fileWalker(lines uint, doResult func(*File)) {
-	var lq *LineQueue
-	if lines != 0 {
-		// not need error check
-		lq, _ = NewLineQueue(lines)
-	}
-	for ; true; w.wg.Done() {
-		file := <-w.fileQueue
-		w.mu.Lock()
-		if _, ok := w.files[file]; ok {
-			w.mu.Unlock()
-			continue
-		}
-		w.files[file] = nil
-		w.mu.Unlock()
-		cs, err := w.readFile(file, lq)
-		if err != nil {
-			w.setInternalError()
-			w.errlog.Printf("%q:%v", file, err)
-			if os.IsNotExist(err) || os.IsPermission(err) {
-				continue
-			}
-			if err == bufio.ErrTooLong {
-				continue
-			}
-			if err == ErrInvalidText {
-				continue
-			}
-			// unexpected error
-			panic(err)
-		}
-		w.log.Println(file)
-		if len(cs) != 0 {
-			doResult(&File{
-				path: file,
-				cs:   cs,
-			})
-		}
-	}
-}
-
+// TODO? readFile(f *File, file string) error
 func (w *Walker) readFile(file string, lq *LineQueue) ([]*Context, error) {
 	f, err := os.Open(file)
 	if err != nil {
